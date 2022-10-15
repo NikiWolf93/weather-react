f5f5adfc4b3ef31017bcc049127f904c20713038
NikiWolf93 <105113354+NikiWolf93@users.noreply.github.com>
2022-10-15 19:08:55 +0300

    axios, loader added, alerts weather

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <h1>Hello world</h1>
+        <Weather city="London" />
+        <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer">
+          {" "}
+          SheCodes
         </a>
       </header>
     </div>
